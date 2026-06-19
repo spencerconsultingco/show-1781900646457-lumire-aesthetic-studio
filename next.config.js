f6be9a8fc200ad517cacd3ac51c849c10647b80c
spencerconsultingco <todd@spencerconsulting.co>
2026-06-19 14:32:05 -0600

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'places.googleapis.com' }, { protocol: 'https', hostname: 'lh3.googleusercontent.com' }, { protocol: 'https', hostname: 'maps.googleapis.com' }] },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'places.googleapis.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'maps.googleapis.com' },
+    ],
+  },
 };
 
 module.exports = nextConfig;
